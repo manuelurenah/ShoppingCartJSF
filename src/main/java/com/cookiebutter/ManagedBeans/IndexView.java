fda2b547aaf370b5bf017fa36fe50a1c12955f83
Manuel E. Ureña <manuel.ureh@hotmail.com>
2016-09-18 18:19:04 -0400

    Initial commit with basic configuration

---
+++ src/main/java/com/cookiebutter/ManagedBeans/IndexView.java
@@ -0,0 +1,17 @@
+package com.cookiebutter.ManagedBeans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+
+/**
+ * Created by MEUrena on 9/18/16.
+ * All rights reserved.
+ */
+@ManagedBean(name = "index")
+@SessionScoped
+public class IndexView implements Serializable {
+
+
+
+}
