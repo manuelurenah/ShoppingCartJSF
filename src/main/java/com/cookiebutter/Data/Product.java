abda70eb57fd4dd401173b4676cb87f149f6f9ef
Luis Rojas <lrojas94@gmail.com>
2016-09-22 02:10:44 -0400

    Added images. Products can now be updated.

--- src/main/java/com/cookiebutter/Data/Product.java
+++ src/main/java/com/cookiebutter/Data/Product.java
@@ -1,11 +1,20 @@
 package com.cookiebutter.Data;
 
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
+import org.primefaces.model.UploadedFile;
+
 import javax.annotation.Generated;
+import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseId;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by luis on 20/09/16.
@@ -19,7 +28,7 @@ public class Product {
     private String name;
     @Column
     private String description;
-    private List<byte[]> images;
+    private ArrayList<UploadedFile> images;
     private User publishedBy;
     private List<String> comments; //Should have their own class. This is on the meantime.
 
@@ -48,20 +57,19 @@ public class Product {
         this.description = description;
     }
 
-    public List<byte[]> getImages() {
-        return images;
-    }
-
-    public void setImages(List<byte[]> images) {
-        this.images = images;
-    }
-
-
     public User getPublishedBy() {
         return publishedBy;
     }
 
     public void setPublishedBy(User publishedBy) {
         this.publishedBy = publishedBy;
+    }
+
+    public ArrayList<UploadedFile> getImages() {
+        return images;
+    }
+
+    public void setImages(ArrayList<UploadedFile> images) {
+        this.images = images;
     }
 }
--- src/main/java/com/cookiebutter/ManagedBeans/ProductsBean.java
+++ src/main/java/com/cookiebutter/ManagedBeans/ProductsBean.java
@@ -2,14 +2,21 @@ package com.cookiebutter.ManagedBeans;
 
 import com.cookiebutter.Data.Product;
 import com.cookiebutter.PersistenceHandlers.ProductService;
+import org.omnifaces.cdi.GraphicImageBean;
 import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
+import org.primefaces.model.UploadedFile;
 
 import javax.ejb.EJB;
-import javax.faces.application.FacesMessage;
+import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseId;
+import java.io.ByteArrayInputStream;
 import java.util.ArrayList;
+import java.util.Optional;
 
 /**
  * Created by luis on 21/09/16.
@@ -23,20 +30,39 @@ public class ProductsBean {
     private int id = -1;
     private String name;
     private String description;
-    private ArrayList<byte[]> images = new ArrayList<byte[]>();
+    private ArrayList<UploadedFile> images = new ArrayList<>();
 
 
 
     public void handleFileUpload(FileUploadEvent event) {
-        System.out.println(event.getFile().getFileName());
-        FacesMessage message = new FacesMessage("Succesful", event.getFile().getFileName() + " was uploaded.");
-        FacesContext.getCurrentInstance().addMessage(null, message);
-        getImages().add(event.getFile().getContents());
+        try {
+            UploadedFile img = event.getFile();
+            getImages().add(img);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
     public ProductService getProductService() {
         return productService;
+    }
+
+    public void clear() {
+        id = -1;
+        name = "";
+        description ="";
+        images = new ArrayList<>();
+    }
+
+    public String editProduct(Product p) {
+        this.name = p.getName();
+        this.id = p.getId();
+        this.description = p.getDescription();
+        this.images = p.getImages();
+
+        return "productForm?faces-redirect=true";
     }
 
     public String processForm() {
@@ -49,7 +75,7 @@ public class ProductsBean {
         }
 
         p.setName(name);
-        p.setImages(images);
+        p.setImages(getImages());
         p.setDescription(description);
         if(id != -1) {
             productService.update(p);
@@ -57,6 +83,7 @@ public class ProductsBean {
         else {
             productService.add(p);
         }
+        clear();
         return "availableProducts?faces-redirect=true";
     }
 
@@ -88,11 +115,70 @@ public class ProductsBean {
         this.description = description;
     }
 
-    public ArrayList<byte[]> getImages() {
+
+    public ArrayList<UploadedFile> getImages() {
         return images;
     }
 
-    public void setImages(ArrayList<byte[]> images) {
+    public void setImages(ArrayList<UploadedFile> images) {
         this.images = images;
+    }
+
+    public StreamedContent getImage(ArrayList<UploadedFile> images) {
+        images = images == null ? this.images : images;
+        FacesContext context = FacesContext.getCurrentInstance();
+
+        if (context.getCurrentPhaseId() == PhaseId.RENDER_RESPONSE) {
+            // So, we're rendering the HTML. Return a stub StreamedContent so that it will generate right URL.
+            return new DefaultStreamedContent();
+        }
+        else {
+            // So, browser is requesting the image. Return a real StreamedContent with the image bytes.
+            String imgName = context.getExternalContext().getRequestParameterMap().get("imgName");
+            Optional<UploadedFile> file = images
+                    .stream()
+                    .filter(uploadedFile -> uploadedFile.getFileName().equals(imgName))
+                    .findFirst();
+
+
+            if(file.isPresent()) {
+                return new DefaultStreamedContent(new ByteArrayInputStream(file.get().getContents()));
+            }
+            else{
+                return new DefaultStreamedContent();
+            }
+        }
+    }
+
+    public void removeImage(UploadedFile image) {
+        images.remove(image);
+    }
+
+    public StreamedContent renderProductImage() {
+        FacesContext context = FacesContext.getCurrentInstance();
+
+        if (context.getCurrentPhaseId() == PhaseId.RENDER_RESPONSE) {
+            // So, we're rendering the HTML. Return a stub StreamedContent so that it will generate right URL.
+            return new DefaultStreamedContent();
+        }
+        else {
+            int productId = Integer.parseInt(context.getExternalContext().getRequestParameterMap().get("productId"));
+            Product p = productService.getById(productId);
+            String imgName = context.getExternalContext().getRequestParameterMap().get("imgName");
+            if(imgName == null) {
+                return new DefaultStreamedContent();
+            }
+            Optional<UploadedFile> file = p.getImages()
+                    .stream()
+                    .filter(uploadedFile -> uploadedFile.getFileName().equals(imgName))
+                    .findFirst();
+
+            if(file.isPresent()) {
+                return new DefaultStreamedContent(new ByteArrayInputStream(file.get().getContents()));
+            }
+            else{
+                return new DefaultStreamedContent();
+            }
+        }
     }
 }
